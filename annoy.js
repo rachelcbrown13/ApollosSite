69f36a917481c022a5a526ae8f919d9bddc3c99e
Rachel Brown <rachelcbrown13@gmail.com>
2020-05-22 15:02:47 -0500

    Color update and cards

---
+++ annoy.js
@@ -0,0 +1,12 @@
+for(i = -10; i <= 19; i++){
+    console.log(i);
+}
+for(i = 10; i <= 40; i += 2){
+    console.log(i);
+}
+for(i = 300; i <= 333; i += 1){
+    if(i % 2 !== 0){
+        console.log(i)
+    }
+}
+for(i = 300; )
